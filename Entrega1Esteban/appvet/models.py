5ad862a37a17c91b9cb88507e3c67941b41b831b
Claudio Esteban <estebanclfe@gmail.com>
2022-11-23 19:35:43 -0300

    mejoramiento buscador, mascotas y modelos

--- Entrega1Esteban/appvet/models.py
+++ Entrega1Esteban/appvet/models.py
@@ -12,6 +12,9 @@ class Mascota(models.Model):
     raza = models.CharField(max_length=20)
     edad = models.IntegerField()
 
+    def __str__(self):
+        return f"{self.especie.capitalize()} - {self.nombre} -  Raza: {self.raza.upper()} - Edad: {self.edad}"
+
 class Producto(models.Model):
     tipo = models.CharField(max_length=20)
     marca = models.CharField(max_length=20)
